bcd4a5ac2f9711518d024e6c79f83b23c2dbfed4
Cooper Johnson <129237833+johnson-cooper@users.noreply.github.com>
2026-09-24 04:29:44 -0400

    PS2: restore src/allocator.h to 3ba129f baseline

--- src/allocator.h
+++ src/allocator.h
@@ -8,15 +8,3 @@ void bump_allocator_free(void);
 void bump_allocator_reset(void);
 void *rs2_malloc(bool use_allocator, int size);
 void *rs2_calloc(bool use_allocator, int count, int size);
-
-#ifdef __PS2__
-// Static-map loc streaming shares the existing scene arena instead of reserving more EE RAM.
-// Permanent scene allocations grow upward; recyclable static-loc allocations grow downward.
-void ps2_loc_allocator_begin(void);
-void ps2_loc_allocator_end(void);
-void ps2_loc_allocator_reset(void);
-int ps2_loc_allocator_used(void);
-int ps2_scene_allocator_used(void);
-int ps2_allocator_gap(void);
-bool ps2_loc_allocator_failed(void);
-#endif
