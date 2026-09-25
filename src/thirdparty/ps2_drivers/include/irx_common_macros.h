0a1930b4cbe925d1d1bcdeab72d69c04c03aab31
Cooper Johnson <129237833+johnson-cooper@users.noreply.github.com>
2026-09-25 11:26:35 -0400

    PS2: vendor ps2_drivers headers

---
+++ src/thirdparty/ps2_drivers/include/irx_common_macros.h
@@ -0,0 +1,38 @@
+#ifndef COMMON_MACROS_H
+#define COMMON_MACROS_H
+
+/// declare IRX ID and return value variables
+#define DECL_IRX_VARS(irx) \
+    int32_t __##irx##_id = -1; \
+    int __##irx##_ret = -1
+
+/// extern IRX ID and return value variables
+#define EXTERN_IRX_VARS(irx) \
+    extern int32_t __##irx##_id; \
+    extern int __##irx##_ret
+
+#define RESET_IRX_VARS(irx) \
+    __##irx##_id = -1; \
+    __##irx##_ret = -1
+
+/* Values taken from PS2SDK
+* MODULE_RESIDENT_END		0
+* MODULE_NO_RESIDENT_END	1
+* MODULE_REMOVABLE_END	2 // Only for 'modload' > v1.2
+*/
+
+/// condition to perform irx load
+#define CHECK_IRX_LOAD(irx) (__##irx##_id == -1) && (__##irx##_ret == -1)
+
+/// condition for irx startup errors
+#define CHECK_IRX_ERR(irx) (__##irx##_id < 0 || __##irx##_ret == 1)
+
+/// check if this irx module is successfully loaded and supports unloading.
+/// It should use just MODULE_REMOVABLE_END, however, in PS2SDK majority of IOP modules return MODULE_RESIDENT_END
+#define CHECK_IRX_UNLOAD(irx) (__##irx##_id >= 0 && (__##irx##_ret == 0 || __##irx##_ret == 2))
+/// extern an embedded irx file buffer and size
+#define EXTERN_IRX(_IRX) \
+extern unsigned char _IRX[] __attribute__((aligned(16))); \
+extern unsigned int size_##_IRX
+
+#endif
---
+++ src/thirdparty/ps2_drivers/include/ps2_filesystem_driver.h
@@ -0,0 +1,61 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright 2005, ps2dev - http://www.ps2dev.org
+# Licenced under GNU Library General Public License version 2
+# Review ps2sdk README & LICENSE files for further details.
+#
+# PS2_FILESYSTEM_DRIVER
+*/
+
+#ifndef PS2_FILESYSTEM_DRIVER
+#define PS2_FILESYSTEM_DRIVER
+
+#include <stdbool.h>
+
+#include <ps2_sio2man_driver.h>
+#include <ps2_fileXio_driver.h>
+#include <ps2_memcard_driver.h>
+#include <ps2_usb_driver.h>
+#include <ps2_mx4sio_driver.h>
+#include <ps2_cdfs_driver.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum BootDeviceIDs {
+    BOOT_DEVICE_UNKNOWN = -1,
+    BOOT_DEVICE_MC0 = 0,
+    BOOT_DEVICE_MC1,
+    BOOT_DEVICE_CDROM,
+    BOOT_DEVICE_CDFS,
+    BOOT_DEVICE_MASS,
+    BOOT_DEVICE_MASS0,
+    BOOT_DEVICE_MASS1,
+    BOOT_DEVICE_MX4SIO,
+    BOOT_DEVICE_MX4SIO0,
+    BOOT_DEVICE_MX4SIO1,
+    BOOT_DEVICE_HDD,
+    BOOT_DEVICE_HDD0,
+    BOOT_DEVICE_HOST,
+    BOOT_DEVICE_HOST0,
+    BOOT_DEVICE_HOST1,
+    BOOT_DEVICE_COUNT,
+};
+
+void init_ps2_filesystem_driver();
+void deinit_ps2_filesystem_driver();
+void init_only_boot_ps2_filesystem_driver();
+void deinit_only_boot_ps2_filesystem_driver();
+bool waitUntilDeviceIsReady(char *path);
+char *rootDevicePath(enum BootDeviceIDs device_id);
+enum BootDeviceIDs getBootDeviceID(char *path);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PS2_FILESYSTEM_DRIVER */
